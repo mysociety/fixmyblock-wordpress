afce29e3d902e01be5eb7ac1b1158ee685177e3d
Zarino Zappia <mail@zarino.co.uk>
2020-03-12 15:17:35 +0000

    Custom post type for Guides

--- fixmyblock-theme/functions.php
+++ fixmyblock-theme/functions.php
@@ -4,3 +4,5 @@ require get_parent_theme_file_path( '/inc/wp-theme-support.php' );
 require get_parent_theme_file_path( '/inc/wp-tidy.php' );
 require get_parent_theme_file_path( '/inc/wp-utilities.php' );
 require get_parent_theme_file_path( '/inc/wp-enqueue.php' );
+
+require get_parent_theme_file_path( '/post-types/guide.php' );
