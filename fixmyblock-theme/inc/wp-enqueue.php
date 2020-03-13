ac65495a47eefaf747bc0746b173d02917052a1a
Zarino Zappia <mail@zarino.co.uk>
2020-03-13 16:18:34 +0000

    Rename main.css to frontend-style.css for consistency

--- fixmyblock-theme/inc/wp-enqueue.php
+++ fixmyblock-theme/inc/wp-enqueue.php
@@ -2,10 +2,10 @@
 
 function enqueue_frontend_stylesheet() {
     wp_enqueue_style(
-        'fixmyblock-style',
-        get_theme_file_uri('/assets/css/main.css'),
+        'fixmyblock-frontend-style',
+        get_theme_file_uri('/assets/css/frontend-style.css'),
         array(),
-        filemtime(get_theme_file_path('/assets/css/main.css'))
+        filemtime(get_theme_file_path('/assets/css/frontend-style.css'))
     );
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_frontend_stylesheet' );
