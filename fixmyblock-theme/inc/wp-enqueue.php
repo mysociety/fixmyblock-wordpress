ce71b1e684d3a6451fc915455a37e46350002483
Zarino Zappia <mail@zarino.co.uk>
2020-03-13 13:05:54 +0000

    Custom admin stylesheet

--- fixmyblock-theme/inc/wp-enqueue.php
+++ fixmyblock-theme/inc/wp-enqueue.php
@@ -1,6 +1,6 @@
 <?php
 
-function scripts_and_styles() {
+function enqueue_frontend_stylesheet() {
     wp_enqueue_style(
         'fixmyblock-style',
         get_theme_file_uri('/assets/css/main.css'),
@@ -8,4 +8,14 @@ function scripts_and_styles() {
         filemtime(get_theme_file_path('/assets/css/main.css'))
     );
 }
-add_action( 'wp_enqueue_scripts', 'scripts_and_styles' );
+add_action( 'wp_enqueue_scripts', 'enqueue_frontend_stylesheet' );
+
+function enqueue_admin_stylesheet() {
+    wp_enqueue_style(
+        'fixmyblock-admin-style',
+        get_theme_file_uri('/assets/css/admin-style.css'),
+        array(),
+        filemtime(get_theme_file_path('/assets/css/admin-style.css'))
+    );
+}
+add_action( 'admin_enqueue_scripts', 'enqueue_admin_stylesheet' );
