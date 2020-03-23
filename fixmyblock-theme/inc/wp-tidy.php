1a205c864b836dcc7f314ee264c23dce37f5b733
Zarino Zappia <mail@zarino.co.uk>
2020-03-23 17:30:58 +0000

    Include pages (not just posts) on tag/category archive pages

--- fixmyblock-theme/inc/wp-tidy.php
+++ fixmyblock-theme/inc/wp-tidy.php
@@ -33,3 +33,20 @@ function enable_full_meta_for_pages() {
     register_taxonomy_for_object_type( 'category', 'page' );
 }
 add_action( 'init', 'enable_full_meta_for_pages' );
+
+
+// Display pages (as well as posts) on category and tag archive pages.
+function modify_post_types_shown_on_taxonomy_archive_pages( $query ) {
+    if ( $query->is_main_query() )  {
+        if ( $query->is_category() || $query->is_tag() ) {
+            $query->set( 'post_type',
+                array(
+                    'post',
+                    'page'
+                )
+            );
+        }
+    }
+    return $query;
+}
+add_filter( 'pre_get_posts', 'modify_post_types_shown_on_taxonomy_archive_pages' );
