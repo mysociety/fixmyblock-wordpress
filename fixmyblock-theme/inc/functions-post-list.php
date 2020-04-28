a5c95255af0ca29328477266640e4e53210b636d
Zarino Zappia <mail@zarino.co.uk>
2020-04-28 11:52:25 +0100

    Display thumbnails in post lists
    Fixes #30.

--- fixmyblock-theme/inc/functions-post-list.php
+++ fixmyblock-theme/inc/functions-post-list.php
@@ -30,6 +30,22 @@ function post_list_item( $post, $args = array() ) {
 
     echo '<div class="post-list__item">' . "\n";
 
+    if ( $a['show_thumbnails'] ) {
+        $thumbnail_html = get_the_post_thumbnail( $post );
+        if ( $thumbnail_html == '' ) {
+            $thumbnail_html = sprintf(
+                '<img src="%s" class="attachment-post-thumbnail size-post-thumbnail wp-post-image" alt="" width="360" height="360">',
+                esc_attr( get_template_directory_uri() . '/assets/img/default-post-thumbnail-360x360.png' )
+            );
+        }
+        echo sprintf(
+            '<div class="post-list__item__image">%s</div>' . "\n",
+            $thumbnail_html
+        );
+    }
+
+    echo '<div class="post-list__item__content">' . "\n";
+
     echo sprintf(
         '<%s class="post-list__item__title"><a href="%s">%s</a></%s>' . "\n",
         esc_html( $a['heading_tag'] ),
@@ -51,5 +67,7 @@ function post_list_item( $post, $args = array() ) {
         );
     }
 
-    echo '</div>';
+    echo '</div>' . "\n";
+
+    echo '</div>' . "\n";
 }
