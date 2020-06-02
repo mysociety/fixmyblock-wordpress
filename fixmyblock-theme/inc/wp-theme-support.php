6d9c82d0b7903160559ea36c48061bdc99f398e1
Zarino Zappia <mail@zarino.co.uk>
2020-06-02 17:36:08 +0100

    Custom colour scheme, with block editor support
    The same colours are now shared between the Bootstrap components
    and the “Colour Settings” sidebar panel in the block editor.
    
    This should make it easier to create call-out boxes on pages using
    colours that match the rest of the site.

--- fixmyblock-theme/inc/wp-theme-support.php
+++ fixmyblock-theme/inc/wp-theme-support.php
@@ -48,3 +48,110 @@ add_theme_support( 'responsive-embeds' );
 
 // https://developer.wordpress.org/themes/functionality/custom-logo/
 add_theme_support( 'custom-logo' );
+
+
+// https://developer.wordpress.org/block-editor/developers/themes/theme-support/#block-color-palettes
+add_theme_support( 'editor-color-palette', array(
+    // Arranged with yellow at the start of the colour wheel,
+    // since that’s the primary site colour.
+    array(
+        'name' => 'Yellow',
+        'slug' => 'yellow',
+        'color' => '#ffe000',
+    ),
+    array(
+        'name' => 'Orange',
+        'slug' => 'orange',
+        'color' => '#FD9B14',
+    ),
+    array(
+        'name' => 'Red',
+        'slug' => 'red',
+        'color' => '#F50400',
+    ),
+    array(
+        'name' => 'Pink',
+        'slug' => 'pink',
+        'color' => '#F6449C',
+    ),
+    array(
+        'name' => 'Blue',
+        'slug' => 'blue',
+        'color' => '#006FD6',
+    ),
+    array(
+        'name' => 'Cyan',
+        'slug' => 'cyan',
+        'color' => '#00B7EB',
+    ),
+    array(
+        'name' => 'Teal',
+        'slug' => 'teal',
+        'color' => '#00CECB',
+    ),
+    array(
+        'name' => 'Green',
+        'slug' => 'green',
+        'color' => '#0EB43A',
+    ),
+    array(
+        'name' => 'White',
+        'slug' => 'white',
+        'color' => '#fff',
+    ),
+    array(
+        'name' => 'Very light gray',
+        'slug' => 'gray-100',
+        'color' => '#F8F9FA',
+    ),
+    array(
+        'name' => 'Mid gray',
+        'slug' => 'gray-600',
+        'color' => '#718590',
+    ),
+    array(
+        'name' => 'Black',
+        'slug' => 'black',
+        'color' => '#000',
+    ),
+    array(
+        'name' => 'Pale yellow',
+        'slug' => 'yellow-100',
+        'color' => '#FFFBD7',
+    ),
+    array(
+        'name' => 'Pale orange',
+        'slug' => 'orange-100',
+        'color' => '#FFF3E1',
+    ),
+    array(
+        'name' => 'Pale red',
+        'slug' => 'red-100',
+        'color' => '#FFE6E6',
+    ),
+    array(
+        'name' => 'Pale pink',
+        'slug' => 'pink-100',
+        'color' => '#FFEEF7',
+    ),
+    array(
+        'name' => 'Pale cyan',
+        'slug' => 'cyan-100',
+        'color' => '#DDF6FD',
+    ),
+    array(
+        'name' => 'Pale blue',
+        'slug' => 'blue-100',
+        'color' => '#EBF5FF',
+    ),
+    array(
+        'name' => 'Pale teal',
+        'slug' => 'teal-100',
+        'color' => '#DCF9F9',
+    ),
+    array(
+        'name' => 'Pale green',
+        'slug' => 'green-100',
+        'color' => '#E4FEEB',
+    ),
+) );
